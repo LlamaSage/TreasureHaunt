dcd71abb73cb40d416221806ffd3200faef0e89f
mvprince <maiaprince@gmail.com>
2019-06-29 17:26:34 +0200

    controller support

---
+++ TreasureHaunt/Assets/FirstPersonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * First person player control.
+ */
+
+public class FirstPersonController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float speed = 6.0f;
+    public float rotationSpeed = 30.0f;
+    public int player;
+    private float rotation;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotation
+        rotation = Input.GetAxis("LeftX_P" + player) * rotationSpeed;
+        transform.Rotate(Vector3.up * rotation * Time.deltaTime);
+
+        // Movement
+        moveDirection = this.transform.forward * Input.GetAxis("LeftY_P" + player) * speed;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
--- TreasureHaunt/Assets/PlayerController.cs
+++ TreasureHaunt/Assets/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
     public float speed = 6.0f;
+    public int player;
     private Vector3 moveDirection = Vector3.zero;
 
     // Start is called before the first frame update
@@ -23,8 +24,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Get direction
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        moveDirection *= speed;
+        moveDirection.x = Input.GetAxis("LeftX_P" + player) * speed;
+        moveDirection.z = Input.GetAxis("LeftY_P" + player) * speed * -1;
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
