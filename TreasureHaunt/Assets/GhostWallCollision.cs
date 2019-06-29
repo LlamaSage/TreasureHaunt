0bff51919af2a1c1101e108e2df141c5ca44072f
Drama Llama <shigure@hotmail.de>
2019-06-29 13:37:05 +0200

    [Add] Ghost Walk Script Ideas

---
+++ TreasureHaunt/Assets/GhostWallCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GhostWallCollision : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("InnerWall");
+        foreach(GameObject go in gos)
+        {
+            Physics.IgnoreCollision(go.GetComponent<Collider>(), this.GetComponent<Collider>(), true);
+        }
+    }
+
+    /*void OnCollisionEnter(Collision c)
+    {
+        Debug.Log(c.gameObject.name+ " AND " +this.gameObject.name);
+        if (c.gameObject.tag == "InnerWall")
+        {
+            Physics.IgnoreCollision(c.collider, GetComponent<Collider>(), true);
+            Debug.Log("TEST");
+        }
+    }*/
+
+
+}
---
+++ TreasureHaunt/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Controls movement.
+ * Src https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float speed = 6.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get direction
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
