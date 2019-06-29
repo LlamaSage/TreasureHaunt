2b589e1418f628478b1540e3e3d53a1b24ef0d2b
mvprince <maiaprince@gmail.com>
2019-06-29 19:58:17 +0200

    basic interaction

--- TreasureHaunt/Assets/InteractableObject.cs
+++ TreasureHaunt/Assets/InteractableObject.cs
@@ -7,9 +7,10 @@ public class InteractableObject : MonoBehaviour
 
     public float openProgress = 0.0f;
     public float openSpeed = 5.0f;
-    private float maxProgress = 100.0f;
+    private float maxProgress = 20.0f;
     private bool isOpen = false;
     private bool beingOpened = false;
+    private bool searchable = true;
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,16 @@ public class InteractableObject : MonoBehaviour
         
     }
 
+    public bool isSearchable()
+    {
+        return searchable;
+    }
     public void setBeingOpened(bool var)
     {
-        beingOpened = var;
+        if (searchable)
+        {
+            beingOpened = var;
+        }
     }
 
     // Update is called once per frame
@@ -30,19 +38,19 @@ public class InteractableObject : MonoBehaviour
             Debug.Log("progress: " + openProgress);
         }
 
-        if (beingOpened & !isOpen)
+        if (beingOpened && searchable)
         {
             openProgress += Time.deltaTime * openSpeed;
         }
         else
         {
             openProgress = 0;
-            beingOpened = false;
         }
 
         if (openProgress >= maxProgress)
         {
             isOpen = true;
+            searchable = false;
         }
     }
 }
