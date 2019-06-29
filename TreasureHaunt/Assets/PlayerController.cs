1c7c445003b08b5329a7a4dde0ba33b8e6ea6dcb
Drama Llama <shigure@hotmail.de>
2019-06-29 17:02:03 +0200

    [Update] Reverting character controls

--- TreasureHaunt/Assets/PlayerController.cs
+++ TreasureHaunt/Assets/PlayerController.cs
@@ -12,7 +12,6 @@ public class PlayerController : MonoBehaviour
     CharacterController characterController;
     public float speed = 6.0f;
     private Vector3 moveDirection = Vector3.zero;
-    public float rotationSpeed = 180.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +22,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0));
         // Get direction
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         moveDirection *= speed;
