5cb23e7203dcc7838b14865f2fa1c1566f89461c
mvprince <maiaprince@gmail.com>
2019-06-30 17:40:09 +0200

    dying animation

---
+++ TreasureHaunt/Assets/ScareArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScareArea : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerController>().IsBeingScared(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerController>().IsBeingScared(false);
+        }
+    }
+}
